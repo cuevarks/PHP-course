da8c3fd239ac63110140b88d8399feff61f63982
Cindy Cuevas <cndycvs@gmail.com>
2019-03-07 17:04:43 -0400

    brrrr

--- index.php
+++ index.php
@@ -1,31 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+<?php
 
-    <style>
-        header {
-            background: gray;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
+$animals = ['khe2', 'khe'];
 
-    <header>
-        <h1>
-            <?php
-            $name = $_GET['name'];
-            echo $name;
-            ?>
-
-        </h1>
-    </header>
-
-</body>
-</html>
+require 'index.view.php';
