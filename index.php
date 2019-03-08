186f586f808312b7c2cc3c1f31812750bb6ec665
Cindy Cuevas <cndycvs@gmail.com>
2019-03-08 17:50:18 -0400

    kinkan 2

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 <?php
 
-$animals = ['khe2', 'khe'];
+class Task{
+
+    public $description;
+    public $completed = false;
+
+    public function __construct($description)
+    {
+        $this->description = $description;
+    }
+
+    public function complete() {
+        $this->completed = true;
+    }
+
+    public function isCompleted()
+    {
+        return $this->completed;
+    }
+
+}
+
+$tasks = [new Task('Do homework'),
+        new Task('Watch courses')];
+
 
 require 'index.view.php';
---
+++ index.view.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <style>
+        header {
+            background: gray;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+
+
+</head>
+<body>
+
+<header>
+       <h1> Testing </h1>
+</header>
+
+    <ul>
+        <?php foreach ($tasks as $task) : ?>
+            <li>
+                <?php if ($task->completed) : ?>
+                    <strike> <?= $task->description; ?> </strike>
+                <?php else: ?>
+                    <?= $task->description; ?>
+                <?php endif; ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+
+</body>
+</html>
