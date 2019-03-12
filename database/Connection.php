71113bec455fd3b16f767c05da9a5c21ef6c94df
Cindy Cuevas <cndycvs@gmail.com>
2019-03-12 11:20:30 -0400

    Changes on the connection file for php

--- database/Connection.php
+++ database/Connection.php
@@ -11,7 +11,9 @@ class Connection
     public static function make()
     {
         try{
-            return new PDO('mysql:localhost;dbname=mytodo','root','');
+            return new PDO('mysql:host=localhost;dbname=mytodo','root','');
+        } catch (PDOException $e) {
+            die($e->getMessage());
         }
     }
 }
